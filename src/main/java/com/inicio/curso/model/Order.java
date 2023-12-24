dde6b492d6494b39bd493356e20269a5227c8e8c
Rafael Souza <rafa.nds13@gmail.com>
2023-12-24 19:28:56 -0300

    Finalizando a classe Order

--- src/main/java/com/inicio/curso/model/Order.java
+++ src/main/java/com/inicio/curso/model/Order.java
@@ -3,8 +3,7 @@ package com.inicio.curso.model;
 import java.io.Serializable;
 import java.time.Instant;
 
-import org.hibernate.annotations.ManyToAny;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -22,6 +21,7 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -35,5 +35,35 @@ public class Order implements Serializable {
         this.moment = moment;
         this.client = client;
     }
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    
     
 }
